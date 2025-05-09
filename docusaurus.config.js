ccb4df47f37b43eba77d2876677baae1bebcb923
Alexander Wang <143363944+EnAmiaN-akioi@users.noreply.github.com>
2025-05-09 13:01:50 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/washu.png',
 
   // Set the production url of your site here
-  url: 'https://wu2rchive.vercel.app',
+  url: 'https://wu2rchive-rng259.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
